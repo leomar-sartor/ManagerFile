8cc662ceb70887e5450c94a87a20e965b6814ee6
Leomar Vaz Sartor <leomar_sartor@unochapeco.edu.br>
2020-03-01 20:49:26 -0300

    Add JQuery, VueJs and Vuetify
    Add functions jquery, vue and vuetify

---
+++ ManagerFile/ManagerFile/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ManagerFile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagerFile.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public IActionResult Index(string node = null)
+        {
+            return View();
+        }
+
+        public IActionResult Vue(string node = null)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
